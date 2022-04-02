e25c63eaf501cad997ea889da222a5dfc8855b1b
Tyler <tylergross28@gmail.com>
2022-04-02 11:12:35 -0400

    button basic setup

--- src/components/adb-button.ts
+++ src/components/adb-button.ts
@@ -1,1 +1,9 @@
-export {};
+import { html, LitElement } from "lit";
+import { customElement } from "lit/decorators.js";
+
+@customElement("adb-button")
+export class AdbButton extends LitElement {
+  render() {
+    return html`<button><slot></slot></button>`;
+  }
+}
