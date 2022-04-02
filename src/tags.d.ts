fb9c3800c75b135669465f8a4f3059405324e086
Tyler <tylergross28@gmail.com>
2022-04-02 11:38:15 -0400

    more minor setup

--- src/tags.d.ts
+++ src/tags.d.ts
@@ -1,3 +1,6 @@
+import { AdbButton } from "./components/adb-button";
+import { AdbProgress } from "./components/adb-progress";
+
 declare global {
   interface HTMLElementTagNameMap {
     "adb-button": AdbButton;
