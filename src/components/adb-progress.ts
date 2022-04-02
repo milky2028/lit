5736aee5e10d37735e4182869105178ab2b6c36b
Tyler <tylergross28@gmail.com>
2022-04-02 11:29:01 -0400

    scaffolding for progress bar

--- src/components/adb-progress.ts
+++ src/components/adb-progress.ts
@@ -1,1 +1,9 @@
-export {};
+import { html, LitElement } from "lit";
+import { customElement } from "lit/decorators.js";
+
+@customElement("adb-progress")
+export class AdbProgress extends LitElement {
+  render() {
+    return html`<div>Progress bar</div>`;
+  }
+}
